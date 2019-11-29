94b73428b5ecc49814dfd6867abf80382321c36b
Anish Kargaonkar <anishkargaonkar@gmail.com>
2019-11-30 04:23:22 +0530

    implement HTML website render

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,84 @@
+import React, { Component, PropsWithChildren, ReactNode } from "react";
+import $ from "jquery";
+import Iframe from 'react-iframe'
+
+type Props = {}
+
+type State = {}
+
+class Editor extends Component<PropsWithChildren<Props>, State> {
+
+  getGeneratedPageURL = ({ html, css, js }: any) => {
+    const getBlobURL = (code: BlobPart, type: string) => {
+      const blob = new Blob([code], { type })
+      return URL.createObjectURL(blob)
+    }
+
+    const cssURL = getBlobURL(css, 'text/css')
+    const jsURL = getBlobURL(js, 'text/javascript')
+
+    const source = `
+      <html>
+        <head>
+          ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
+          ${js && `<script src="${jsURL}"></script>`}
+        </head>
+        <body>
+          ${html || ''}
+        </body>
+      </html>
+    `
+
+    return getBlobURL(source, 'text/html')
+  }
+
+  injectIframeContent = () => {
+
+    const fetchHtmlData = async () => {
+      const res = await fetch('https://jsonp.afeld.me/?url=https://jsonview.com/example.json');
+      const html = await res.text()
+      return html;
+      // return JSON.stringify()
+    }
+
+    const injectScript = () => `
+      // window.location.href = "https://www.google.com/"
+      fetch('https://jsonp.afeld.me/?url=https://stackoverflow.com')  
+        .then(response => response.text())  
+        .then(html => {
+          // console.log(html);
+          document.body.innerHTML = html;
+
+          $('body').children().mouseover(function (e) {
+            $(".hova").removeClass("hova");
+            $(e.target).addClass("hova");
+            return false;
+          }).mouseout(function (e) {
+            $(window).removeClass("hova");
+          });
+        })
+    `;
+
+    const injectCss = () => `
+    .hova {
+      background-color: pink !important;
+    }
+    `;
+
+    return this.getGeneratedPageURL({
+      html: fetchHtmlData(),
+      css: injectCss(),
+      js: injectScript()
+    })
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <iframe style={{ height: "100% !important", width: "100% !important" }} id="myIFrame" src={this.injectIframeContent()} />
+    )
+  }
+}
+
+export default Editor;
